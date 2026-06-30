403fb034091e6302fb0b4d6c1b4fbe1aabc748bf
belusuarez13 <86313972+belusuarez13@users.noreply.github.com>
2026-06-29 21:05:33 -0300

    Delete test_id.py

--- test_id.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-TOKEN = "8747785658:AAHsNJyy604ChFyGMFhBNuFM7HPmA2j2zSI"
-URL = f"https://api.telegram.org/bot{TOKEN}"
-
-updates = requests.get(f"{URL}/getUpdates").json()
-print(updates)
